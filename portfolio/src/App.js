3523facb2bb3a9fea2159424f01cc445ea2f8a0e
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2022-05-03 17:51:57 -0500

    Added About Me page using props to change the state for other pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../src/components/Header";
 import Page from "../src/components/Page"
+import currentPage from "../src/components/Nav"
 
 function App() {
   return (
   <div>
     <Header/>
-    <Page />
+    <main>
+    <Page currentPage={currentPage}></Page>
+    </main>
     
   </div>
   );
--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 import Nav from "../Nav";
 
 function Header() {
+    const [pages] = useState([
+        {
+            name: "about me"
+        },
+        { name: "portfolio" },
+        { name: "contact" },
+        {
+            name: "resume"
+        }
+    ]);
+
+    const [currentPage, setCurrentPage] = useState(pages[0]);
+
     return (
-    <header>
-        <h1 className='name'>Jesse Alvarez</h1>
-        <Nav />
-    </header>
+        <header>
+            <h1 className='name'>Jesse Alvarez</h1>
+            <Nav
+                pages={pages}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            ></Nav>
+        </header>
     );
 }
 
