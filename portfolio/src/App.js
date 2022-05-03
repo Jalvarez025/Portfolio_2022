5b29c161c47d8841a2ae221f3caf96df6ea122f8
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2022-05-03 17:33:53 -0500

    Added Page folder

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,57 +1,15 @@
 import React from "react";
 import Header from "../src/components/Header";
-//import Page from "../src/components/Page"
-//import Nav from "../src/components/Nav"
+import Page from "../src/components/Page"
 
 function App() {
   return (
   <div>
     <Header/>
-    
+    <Page />
     
   </div>
   );
 }
 
 export default App;
-
-
-
-// import React, { useState } from "react";
-// import Header from "./components/Header";
-// import Nav from "./components/Nav";
-// import Page from "./components/Page";
-// import Footer from "./components/Footer";
-
-// function App() {
-//   const [pages] = useState([
-//     {
-//       name: "about me"
-//     },
-//     { name: "portfolio" },
-//     { name: "contact" },
-//     {
-//       name: "resume"
-//     }
-//   ]);
-
-//   const [currentPage, setCurrentPage] = useState(pages[0]);
-
-//   return (
-//     <div>
-//       <Header>
-//         <Nav
-//           pages={pages}
-//           setCurrentPage={setCurrentPage}
-//           currentPage={currentPage}
-//         ></Nav>
-//       </Header>
-//       <main>
-//         <Page currentPage={currentPage}></Page>
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
---
+++ portfolio/src/components/Page/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page(){
+    return (
+        <body>
+            <p>Hello add intro text for About Me page!!</p>
+        </body>
+    );
+}
+
+export default Page;
